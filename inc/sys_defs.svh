53e2ad783d44c6f971d5e3e824f89aa2e3d8eab7
dws1 <dwvshep@umich.edu>
2025-11-17 23:45:59 -0500

    sysdefs tyoes added

--- inc/sys_defs.svh
+++ inc/sys_defs.svh
@@ -146,7 +146,7 @@
 // ---- Fixed Point Type Definitions ---- //
 ////////////////////////////////////////////
 
-// We can reduce the fractional bits at any time to save space and compromise on precision
+// We can reduce the fractional bits based on the most needed precision to keep the true Q0.7 at the output
 
 //---------------------------
 // Weights and Outputs QTypes
@@ -162,63 +162,86 @@ typedef `Q_TYPE(0, 7) MEM_QT;
 
 //IDK THE BEST WAY TO STORE INTERMEDIATES IN A PROGRAMMABLE WAY FOR THE LOG FOR LOOP
 // Product of two qmem_t's
-typedef `Q_TYPE(0, 14) PRODUCT_QT;
-
-// Sum of two qproduct_t's
-typedef `Q_TYPE(1, 14) SUM2_QT;
+//typedef `Q_TYPE(1, 14) PRODUCT_QT;
 
 // Dot-product output: Q6.14 (64-wide dot products)
-typedef `Q_TYPE(6, 14) DOT_QT;
+//typedef `Q_TYPE(7, 14) DOT_QT;
 
-// Logits (QKᵀ scaled by 1/√d a.k.a >>> 3): Q3.17
-typedef `Q_TYPE(3, 17) DOT_SCALED_QT;
+// Logits (QKᵀ scaled by 1/√d a.k.a >>> 3): Q4.17
+//typedef `Q_TYPE(4, 17) DOT_SCALED_QT;
 
 
 //-------------------
 // Expmul QTypes
 //-------------------
 
-// Difference of the two Q3.17 inputs
-typedef `Q_TYPE(4, 17) EXPMUL_DIFF_QT
+// Difference of the two Q4.17 inputs
+//typedef `Q_TYPE(5, 17) EXPMUL_DIFF_QT
 
 // Product of Q4.17 with log2e via approximation (x + x >> 1 - x >> 4)
-typedef `Q_TYPE(5, 21) EXPMUL_LOG2E_QT
+//typedef `Q_TYPE(6, 21) EXPMUL_LOG2E_QT
 
-// Clipped exponent based on the Q5.21
-typedef `Q_TYPE(5, 0) EXPMUL_EXPONENT_QT
+// Clipped exponent based on the Q6.21 to the range (-16, 15) **Guaranteed to be negative
+//typedef `Q_TYPE(4, 0) EXPMUL_EXPONENT_QT
 
-// 2^-L * V output format (right shift by 0-15 bits): Q0.22
-// Must cast input V weights to this many fractional bits plus 1 for representing "1" appended to the beginning 
+// 2^-L * V output format (right shift by 0-16 bits): Q0.23
+// Must cast input V weights to this many fractional bits plus 1 int bit for representing "1" appended to the beginning 
 // and plus another 9 for the 512 accumulations into the output vetor
-typedef `Q_TYPE(9, 22) EXPMUL_VSHIFT_QT;
-// If we are considering 1 as a value in the V vector, technically each value is a Q1.7, and the output would be a Q1.22
+// If we are considering 1 as a value in the V vector, technically each value is a Q1.7, and the output would be a Q1.23
 // Once we add 512 of these together we will add 9 bits to the integer portion and therefore
-// The output would be Q10.22, but we can assume we will never really hit that max and just keep it at Q9.22
+// The output would be Q10.23, but we can assume we will never really hit that max and just keep it at Q9.23
+//typedef `Q_TYPE(9, 23) EXPMUL_VSHIFT_QT;
 
-//All inputs to the div module are therefore Q9.22's (32 bits)
+//All inputs to the div module are therefore Q9.23's (33 bits)
 //The outputs should be back to Q0.7s, and this should not require an integer bit clipping since the 
-//output values should all be less than 1.
+//output values should all be less than 1 after division
 
+//If we know we only need 7 fractional bits of precision at the end of the day,
+//We can throw away some fractional bits in the middle of the pipeline if they cannot
+//possibly effect the value of the final output
+
+//Now we work backwards from division
+
+//Since we know the denominator should be at least 1, 
+//Chat math says we should only need one extra fractional bit in the
+//numerator and denominator to prevent and errors in the output Q0.7
+typedef `Q_TYPE(9, 8) EXPMUL_VSHIFT_QT;
+
+// Keep the possible shift exponent rang (-16, 15) this is almost overkill
+typedef `Q_TYPE(4, 0) EXPMUL_EXPONENT_QT
+
+//Since we clip to an int, we only need to keep one fractional bit from the Q6.21
+typedef `Q_TYPE(6, 1) EXPMUL_LOG2E_QT
+
+//The same goes for the difference which gets multiplied by 1.43, but we can keep an extra bit for fun (8 bits total)
+typedef `Q_TYPE(5, 2) EXPMUL_DIFF_QT
+
+//For linear ops like subtraction, just keep adding one more frac bit
+typedef `Q_TYPE(4, 3) SCORE_QT;
+
+//Before the shift of dividing by root(dk) we just need 7 int bits if we want to get a Q4.3 out
+typedef `Q_TYPE(7, 6) DOT_QT;
+
+//Need partial sum support here
+typedef `Q_TYPE(4, 6) PARTIAL_DOT_QT;
+
+//Since the above output comes from an essential shift by 6 we can say:
+typedef `Q_TYPE(1, 6) PRODUCT_QT;
 
 
 ////////////////////////////////////
 // ---- I/O Type Definitions ---- //
 ////////////////////////////////////
 
-typedef logic signed [`INTEGER_WIDTH-1:0] INT_T;
+typedef MEM_QT [`MAX_EMBEDDING_DIM] Q_VECTOR_T;
 
-typedef INT_T [`MAX_EMBEDDING_DIM] Q_VECTOR_T;
+typedef MEM_QT [`MAX_EMBEDDING_DIM] K_VECTOR_T;
 
-typedef INT_T [`MAX_EMBEDDING_DIM] K_VECTOR_T;
+typedef MEM_QT [`MAX_EMBEDDING_DIM] V_VECTOR_T;
 
-typedef INT_T [`MAX_EMBEDDING_DIM] V_VECTOR_T;
+typedef MEM_QT [`MAX_EMBEDDING_DIM] O_VECTOR_T;
 
-typedef INT_T [`MAX_EMBEDDING_DIM] O_VECTOR_T;
-
-typedef  [`MAX_EMBEDDING_DIM+1][`DOT_PRODUCT_SIZE-1:0] STAR_VECTOR_T_IN; //Input to expmul
-
-typedef  [`MAX_EMBEDDING_DIM+1][`EXPMUL_OUTPUT_SIZE-1:0] STAR_VECTOR_T_OUT; //Append 1 to the vector to store l in the output, output from expmul
-
+typedef EXPMUL_VSHIFT_QT [`MAX_EMBEDDING_DIM+1] STAR_VECTOR_T;
 
 
 
