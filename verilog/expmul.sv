0644ac91cba68700b9e2b5d79533687b6252fe7b
Jeffrey <yangje@umich.edu>
2025-11-19 00:24:29 -0500

    expmul complete, needs debugging, q_convert needs to be changed to real thing

--- verilog/expmul.sv
+++ verilog/expmul.sv
@@ -10,92 +10,92 @@ module expmul(
     output rdy_out,
 
     //Data signals
-    input INT_T m_in,
-    input INT_T m_prev_in,
-    input STAR_VECTOR_T o_star_prev_in,
-    input INT_T s_in,
-    input STAR_VECTOR_T v_star_in,
-    output STAR_VECTOR_T exp_v_out,
-    output STAR_VECTOR_T exp_o_out
+    input SCORE_QT m_in,
+    input SCORE_QT m_prev_in,
+    input EXPMUL_VSHIFT_QT o_star_prev_in,
+    input SCORE_QT s_in,
+    input EXPMUL_VSHIFT_QT v_star_in,
+    output EXPMUL_VSHIFT_QT exp_v_out,
+    output EXPMUL_VSHIFT_QT exp_o_out
 );
 
     //Internal Pipeline Registers
-    INT_T m,
-    INT_T m_prev;
-    STAR_VECTOR_T o_star_prev;
-    INT_T s;
-    STAR_VECTOR_T v_star;
-    logic valid_reg;
+    SCORE_QT m,
+    SCORE_QT m_prev;
+    SCORE_QT s;
+    EXPMUL_VSHIFT_QT o_star_prev;
+    EXPMUL_VSHIFT_QT v_star;
+    logic expmul_o_valid, expmul_o_rdy, expmul_v_valid, expmul_v_rdy;
 
-    assign vld_out = valid_reg;
-    assign rdy_out = rdy_in || !valid_reg;
+    assign vld_out = expmul_o_valid && expmul_v_valid;
+    assign rdy_out = (expmul_o_rdy && expmul_v_rdy) || !(expmul_o_valid && expmul_v_valid);
 
     //Latch inputs first
-    always_ff @(posedge clk) begin
-        if(rst) begin
-            m <= '0;
-            m_prev <= '0;
-            o_star_prev <= '0;
-            s <= '0;
-            v_star <= '0;
-            valid_reg <= 1'b0;
-        end else begin
-            if(vld_in && rdy_out) begin //Handshake successful
-                m <= m_in;
-                m_prev <= m_prev_in;
-                o_star_prev <= o_star_prev_in;
-                s <= s_in;
-                v_star <= v_star_in;
-                valid_reg <= 1'b1;
-            end else if(rdy_in) begin //Only downstream is ready (clear internal pipeline)
-                valid_reg <= 1'b0;
-            end
-        end
-    end
+    // always_ff @(posedge clk) begin
+    //     if(rst) begin
+    //         m <= '0;
+    //         m_prev <= '0;
+    //         o_star_prev <= '0;
+    //         s <= '0;
+    //         v_star <= '0;
+    //         valid_reg <= 1'b0;
+    //     end else begin
+    //         if(vld_in && rdy_out) begin //Handshake successful
+    //             m <= m_in;
+    //             m_prev <= m_prev_in;
+    //             o_star_prev <= o_star_prev_in;
+    //             s <= s_in;
+    //             v_star <= v_star_in;
+    //             valid_reg <= 1'b1;
+    //         end else if(rdy_in) begin //Only downstream is ready (clear internal pipeline)
+    //             valid_reg <= 1'b0;
+    //         end
+    //     end
+    // end
 
 
     //Use these blocks if expmul is combinational
-    expmul_comb expmul_o_inst (
-        .a_in(m_prev),
-        .b_in(m),
-        .v_in(o_star_prev),
-        .v_out(exp_o_out)
-    );
-
-    expmul_comb expmul_v_inst (
-        .a_in(s),
-        .b_in(m),
-        .v_in(v_star),
-        .v_out(exp_v_out)
-    );
-
-    //Use these if expmul is pipelined
-    //But remember to not latch inputs in this top module so you dont waste an extra cycle
-    //Also add support for internal valid-ready signals
-    // expmul_stage expmul_o_inst (
-    //     .clk(clk),
-    //     .rst(rst),
-    //     .vld_in(vld_in),
-    //     .rdy_in(rdy_in),
-    //     .vld_out(),
-    //     .rdy_out(),
+    // expmul_comb expmul_o_inst (
     //     .a_in(m_prev),
     //     .b_in(m),
     //     .v_in(o_star_prev),
     //     .v_out(exp_o_out)
     // );
 
-    // expmul_stage expmul_v_inst (
-    //     .clk(clk),
-    //     .rst(rst),
-    //     .vld_in(vld_in),
-    //     .rdy_in(rdy_in),
-    //     .vld_out(),
-    //     .rdy_out(),
+    // expmul_comb expmul_v_inst (
     //     .a_in(s),
     //     .b_in(m),
     //     .v_in(v_star),
     //     .v_out(exp_v_out)
+    // );
+
+    //Use these if expmul is pipelined
+    //But remember to not latch inputs in this top module so you dont waste an extra cycle
+    //Also add support for internal valid-ready signals
+    expmul_stage expmul_o_inst (
+        .clk(clk),
+        .rst(rst),
+        .vld_in(vld_in),
+        .rdy_in(rdy_in),
+        .vld_out(expmul_o_valid),
+        .rdy_out(expmul_o_rdy),
+        .a_in(m_prev_in),
+        .b_in(m_in),
+        .v_in(o_star_prev_in),
+        .v_out(exp_o_out)
+    );
+
+    expmul_stage expmul_v_inst (
+        .clk(clk),
+        .rst(rst),
+        .vld_in(vld_in),
+        .rdy_in(rdy_in),
+        .vld_out(expmul_v_valid),
+        .rdy_out(expmul_v_rdy),
+        .a_in(s_in),
+        .b_in(m_in),
+        .v_in(v_star_in),
+        .v_out(exp_v_out)
     // );
 
 
--- verilog/expmul_stage.sv
+++ verilog/expmul_stage.sv
@@ -18,7 +18,7 @@ Stage 2:
     and this formula: n = x >> 4 is the same as n = x
 */
 
-module expmul_stage(
+module expmul_stage (
     //control signals
     input clk,
     input rst,
@@ -30,63 +30,89 @@ module expmul_stage(
     output rdy_out,
 
     //Data signals
-    input [`DOT_PRODUCT_SIZE-1:0] a_in,
-    input [`DOT_PRODUCT_SIZE-1:0] b_in,
-    input STAR_VECTOR_T_IN v_in,
-    output STAR_VECTOR_T_OUT v_out
+    input SCORE_QT a_in, //Q4.3
+    input SCORE_QT b_in, //Q4.3
+    input EXPMUL_VSHIFT_QT v_in, //Q9.8
+    output EXPMUL_VSHIFT_QT v_out //Q9.8
 );
 
     //Internal Pipeline Registers
-    INT_T a;
-    INT_T b;
-    INT_T x;
-    STAR_VECTOR_T v;
-    logic valid_reg;
-    INT_T l_hat, l_hat_next;
-    logic [`MAX_EMBEDDING_DIM+1][`INTEGER_WIDTH+15:0] shift_stage_1_result;
-    logic [`MAX_EMBEDDING_DIM+1][`INTEGER_WIDTH+23:0] shift_stage_2_result;
-    logic [`MAX_EMBEDDING_DIM+1][`INTEGER_WIDTH+27:0] shift_stage_3_result;
-    logic [`MAX_EMBEDDING_DIM+1][`INTEGER_WIDTH+29:0] shift_stage_4_result;
-    logic [`MAX_EMBEDDING_DIM+1][`INTEGER_WIDTH+30:0] shift_stage_5_result;
+    SCORE_QT a;
+    SCORE_QT b;
+    `Q_TYPE(5, 3) x_diff;
+    `Q_TYPE(5, 2) x_diff_condensed;
+    `Q_TYPE(6, 6) log_e_x;
+    `Q_TYPE(6, 1) log_e_x_condensed;
+    EXPMUL_VSHIFT_QT v;
+    logic stage_1_valid, stage_2_valid, stage_1_ready, stage_2_ready;
+    `Q_TYPE(4, 0) l_hat, l_hat_next;
+    EXPMUL_VSHIFT_QT [`MAX_EMBEDDING_DIM+1] shift_stage_1_result; //Q9.16
+    EXPMUL_VSHIFT_QT [`MAX_EMBEDDING_DIM+1] shift_stage_2_result; //Q9.16
+    EXPMUL_VSHIFT_QT [`MAX_EMBEDDING_DIM+1] shift_stage_3_result; //Q9.16
+    EXPMUL_VSHIFT_QT [`MAX_EMBEDDING_DIM+1] shift_stage_4_result; //Q9.16
+    EXPMUL_VSHIFT_QT [`MAX_EMBEDDING_DIM+1] shift_stage_5_result; //Q9.16
 
-    INT_T int_portion_x;
-
-    assign vld_out = valid_reg;
-    assign rdy_out = rdy_in || !valid_reg;
-    assign x = a - b;
-    assign int_portion_x = x >> 7; //remove the bits that represent the "decimal" portion of the real unquantized value, value of shift is subject
-                                   //to change depending on format of incoming data
+    assign stage_2_ready = rdy_in;
+    assign stage_1_ready = (!stage_1_valid) || stage_2_ready;
+    assign rdy_out = stage_1_ready;
+    assign vld_out = stage_2_valid;
+    assign x_diff = a - b;
+    assign x_diff_condensed = q_convert(5, 3, 5, 2); // Convert from Q5.3 to Q5.2
+    
+    //remove the bits that represent the "decimal" portion of the real unquantized value, value of shift is subject
+    //to change depending on format of incoming data
 
     //Latch inputs first
+    //First stage: Diff and then do log2e*X approximation
     always_ff @(posedge clk) begin
         if(rst) begin
             a <= '0;
             b <= '0;
             v <= '0;
             x <= '0;
-            valid_reg <= 1'b0;
-        end else begin
-            if(vld_in && rdy_out) begin //Handshake successful
+            stage_1_valid <= 1'b0;
+        end else begin //Handshake successful
+            if (vld_in && stage_1_ready) begin
                 a <= a_in;
                 b <= b_in;
                 v <= v_in;
-                valid_reg <= 1'b1;
-            end else if(rdy_in) begin //Only downstream is ready (clear internal pipeline)
-                valid_reg <= 1'b0;
+                stage_1_valid <= 1'b1;
+        end else if (stage_1_ready) begin
+            stage_1_valid <= 0;
+        end
+        end
+    end
+
+    //Second stage: 2^-L * V
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            stage_2_valid <= 1'b0;
+        end else begin
+            if (stage_1_valid && stage_2_ready) begin
+                stage_2_valid <= 1'b1;
+                l_hat <= l_hat_next;
+            end else if (stage_2_ready) begin
+                stage_2_valid <= 1'b0;
             end
-            l_hat <= l_hat_next;
         end
     end
 
     //output is combinational
     always_comb begin
-        l_hat_next = int_portion_x + int_portion_x >> 1 - int_portion_x >> 4;
-    end 
+        log_e_x = {1'b0, x_diff_condensed, 4'b0000} + {1'b0, ({x_diff_condensed, 4'b0000} >> 1)} - {1'b0, ({x_diff_condensed, 4'b0000} >> 4)};
+        l_hat_next = q_convert(); //Convert Q6.6 to Q4.0
+        //l_hat_next = log_e_x[12] : {1'b1, log_e_x[6 +: 4]} : '0;
+    end
 
     generate 
         for (genvar i = 0; i < `MAX_EMBEDDING_DIM+1; i++) begin
-            assign shift_stage_1_result[i] = l_hat[4] ? {v[i], 16'h0000} >> 16 : {v[i], 4'h0000};
-            assign shift_stage_2_result[i] = l_hat[3] ? {v[i], 8'b00000000}
-
+            assign shift_stage_1_result[i] = l_hat[4] ? {v[i], 16'h0000} >> 16 : {v[i], 16'h0000};
+            assign shift_stage_2_result[i] = l_hat[3] ? {v[i], 8'b00000000} >> 8 : {v[i], 8'h00};
+            assign shift_stage_3_result[i] = l_hat[2] ? {v[i], 4'b0000} >> 4 : {v[i], 4'b0000};
+            assign shift_stage_4_result[i] = l_hat[1] ? {v[i], 2'b00} >> 2 : {v[i], 2'b00};
+            assign shift_stage_5_result[i] = l_hat[0] ? {v[i], 1'b0} >> 1 : {v[i], 1'b0};
+            assign v_out[i] = q_convert(); //Convert Q9.23 to Q9.7
+        end
+    endgenerate
 
 endmodule
